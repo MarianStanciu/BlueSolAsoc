f4d71544651b987ec44d6d78bf363b750323d4b5
Marian <stanciu.m@gmail.com>
2020-10-28 10:09:07 +0200

    bb

---
+++ CasetaDialog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class CasetaDialog: MessageBox
+
+{
+	public CasetaDialog ()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+
+}
